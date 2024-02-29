f1a4a3a853403a1c58211c0f8a6f717284bbeb80
emre fidan <emrefidan_763@hotmail.com>
2024-02-29 23:21:22 +0300

    Employe Eklendi

---
+++ MVCGitSimulations/Controllers/EmployesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGitSimulations.Controllers
+{
+    public class EmployesController : Controller
+    {
+        // GET: Employes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
